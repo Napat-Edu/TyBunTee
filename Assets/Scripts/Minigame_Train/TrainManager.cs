5381ea9d1f4a8a41f1596cfdee1c948967143d1c
Napat-Edu <omegyzr@gmail.com>
2023-11-16 16:53:40 +0700

    [UPDATE] add random train function

--- Assets/Scripts/Minigame_Train/TrainManager.cs
+++ Assets/Scripts/Minigame_Train/TrainManager.cs
@@ -10,13 +10,46 @@ public class TrainManager : MonoBehaviour
     [SerializeField] Button saveRightTrainButton;
 
     [SerializeField] GameObject trainSection;
+    [SerializeField] GameObject normalTrain;
+    [SerializeField] GameObject crashedTrain;
 
-    int currentTrainIndex;
+    [SerializeField] Transform trainInitialPosition;
 
     void Awake()
     {
-        currentTrainIndex = 0;
-        GameObject gameObject = new();
-        gameObject.transform.parent = trainSection.transform;
+        InitTrainSection(0);
+    }
+
+    void InitTrainSection(int mode)
+    {
+        int trainAmount = 0;
+        int crashedAmount = 0;
+
+        if (mode == 0)
+        {
+            // easy mode
+            trainAmount = 4;
+            crashedAmount = 1;
+        }
+
+        int currentCrashedAmount = 0;
+        Vector3 genTrainPosition = trainInitialPosition.transform.position;
+        for (int i = 0; i < trainAmount; i++)
+        {
+            GameObject newGameObject;
+
+            if (currentCrashedAmount != crashedAmount && (Random.Range(0, 1.0f) < 0.5 || ((i + 1) + crashedAmount >= trainAmount)))
+            {
+                newGameObject = Instantiate(crashedTrain, genTrainPosition, Quaternion.identity);
+                currentCrashedAmount += 1;
+            }
+            else
+            {
+                newGameObject = Instantiate(normalTrain, genTrainPosition, Quaternion.identity);
+            }
+
+            newGameObject.transform.SetParent(trainSection.transform);
+            genTrainPosition += new Vector3(200, 0, 0);
+        }
     }
 }
