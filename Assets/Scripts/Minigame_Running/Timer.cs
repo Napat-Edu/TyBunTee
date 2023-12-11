8122cb10e56891121de53e3c94c3eab03de34164
Atthaset Niyomsri <94776045+Atthaset@users.noreply.github.com>
2023-12-11 22:39:36 +0700

    TestTimer

---
+++ Assets/Scripts/Minigame_Running/Timer.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI timerText;
+    [SerializeField] float remainingTime = 10;
+
+    // Update is called once per frame
+    void Update()
+{
+    if (remainingTime > 0)
+    {
+        remainingTime -= Time.deltaTime;
+
+        // Extract seconds and milliseconds
+        int seconds = Mathf.FloorToInt(remainingTime);
+        int milliseconds = Mathf.FloorToInt((remainingTime - seconds) * 1000);
+
+        timerText.text = string.Format("{0:00}:{1:00}", seconds, milliseconds / 10);
+    }
+    else if (remainingTime < 0)
+    {
+        remainingTime = 0;
+        timerText.color = Color.red;
+        timerText.text = "00:00"; // Display 00:00 when time's up
+    }
+}
+}
