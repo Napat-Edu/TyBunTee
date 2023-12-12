64579f602641eb5ca3caeb39338a3e36b264e2d4
Atthaset Niyomsri <94776045+Atthaset@users.noreply.github.com>
2023-12-12 23:49:54 +0700

    12/12/2023

--- Assets/Scripts/Minigame_Running/Draggable.cs
+++ Assets/Scripts/Minigame_Running/Draggable.cs
@@ -3,28 +3,51 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class DraggableItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
-{
+public class DraggableItem : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler, IDropHandler {
 
-    Transform parentAfterDrag;
+    [SerializeField] private Canvas canvas;
+    public RectTransform rectTransform;
 
-    public void OnBeginDrag(PointerEventData eventData)
-    {
+    private CanvasGroup canvasGroup;
+
+    private Vector2 startPosition;
+
+    private void Awake(){
+        rectTransform = GetComponent<RectTransform>();
+        canvasGroup = GetComponent<CanvasGroup>();
+        startPosition = rectTransform.anchoredPosition;
+    }
+
+    public void OnBeginDrag(PointerEventData eventData) {
         Debug.Log("BeginDrag");
-        parentAfterDrag = transform.parent;
-        transform.SetParent(transform.root);
-        transform.SetAsLastSibling();
+        canvasGroup.alpha = .6f;
+        canvasGroup.blocksRaycasts = false;
     }
 
-    public void OnDrag(PointerEventData eventData)
-    {
+    public void OnDrag(PointerEventData eventData) {
         Debug.Log("OnDrag");
-        transform.position = Input.mousePosition;
+        rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
     }
 
-    public void OnEndDrag(PointerEventData eventData)
-    {
-        Debug.Log("OnEndDrag");
-        transform.SetParent(parentAfterDrag);
+    public void OnEndDrag(PointerEventData eventData) {
+        Debug.Log("EndDrag");
+        canvasGroup.alpha = 1f;
+        canvasGroup.blocksRaycasts = true;
+
+        ItemSlot droppedSlot = eventData.pointerCurrentRaycast.gameObject.GetComponent<ItemSlot>();
+        if (droppedSlot == null && rectTransform.anchoredPosition != startPosition) {
+            rectTransform.anchoredPosition = startPosition;
+         }
+    }
+
+    public void OnPointerDown(PointerEventData eventData) {
+        Debug.Log("PointerDown");
+    }
+
+    public void OnDrop(PointerEventData eventData) {
+        Debug.Log("OnDrop");
     }
 }
+
+
+
