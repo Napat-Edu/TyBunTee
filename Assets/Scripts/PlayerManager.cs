5ca11098fd682a1c2de6609e2722b2d13766f8f4
Napat-Edu <omegyzr@gmail.com>
2023-12-13 17:27:27 +0700

    random minigame and delay

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -1,6 +1,7 @@
-using Microsoft.Unity.VisualStudio.Editor;
+using System.Collections;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerManager : MonoBehaviour
 {
@@ -12,6 +13,9 @@ public class PlayerManager : MonoBehaviour
 
     [SerializeField] GameObject EndgamePopup;
 
+    [SerializeField] Transform[] framePosition;
+    [SerializeField] GameObject SelectedFrame;
+
     int[] playerScore;
 
     void Awake()
@@ -22,7 +26,12 @@ public class PlayerManager : MonoBehaviour
         scoreManager.CountTurn();
 
         difficulty.text = SetDifficultText();
-        CheckEndgameCondition();
+        bool isPlayerWinExist = CheckEndgameCondition();
+
+        if (!isPlayerWinExist)
+        {
+            RandomMiniGame();
+        }
     }
 
     void InitPlayerScore()
@@ -56,7 +65,7 @@ public class PlayerManager : MonoBehaviour
         return baseText;
     }
 
-    void CheckEndgameCondition()
+    bool CheckEndgameCondition()
     {
         bool isPlayerWinExist = false;
         int topScore = 0;
@@ -84,11 +93,43 @@ public class PlayerManager : MonoBehaviour
             }
             OpenEndGameDiaogue(topScore);
         }
+
+        return isPlayerWinExist;
     }
 
     void OpenEndGameDiaogue(int topScore)
     {
         topScoreText.text = "Top Score : " + topScore;
         EndgamePopup.SetActive(true);
+    }
+
+    void RandomMiniGame()
+    {
+        int minigame = Random.Range(1, 4);
+
+        StartCoroutine(WaitForMinigame(minigame));
+    }
+
+    IEnumerator WaitForMinigame(int minigame)
+    {
+        yield return new WaitForSeconds(1);
+
+        SelectedFrame.transform.position = framePosition[minigame - 1].transform.position;
+        SelectedFrame.SetActive(true);
+
+        yield return new WaitForSeconds(2);
+
+        if (minigame == 1)
+        {
+            SceneManager.LoadScene("Minigame_Train");
+        }
+        else if (minigame == 2)
+        {
+            SceneManager.LoadScene("Temp_Running");
+        }
+        else
+        {
+            SceneManager.LoadScene("Fall_Game");
+        }
     }
 }
